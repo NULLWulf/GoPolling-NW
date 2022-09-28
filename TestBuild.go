8031e720c336a638ed063e32a439f004c5339bd0
nullwulf <nwolf2@oswego.edu>
2022-09-28 19:17:25 -0400

    testing API env key calls

--- TestBuild.go
+++ TestBuild.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/nullwulf/loggly"
+	"os"
+	//loggly "github.com/nullwulf/loggly"
+)
 
 func main() {
-	fmt.Println("Classically Hello")
+
+	//top10CryptoUrl := "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=10"
+	tag := "Top10Cryptos"
+
+	client := loggly.New(tag)
+	fmt.Println(client)
+	fmt.Println(os.Getenv("LOGGLY_TOKEN"))
+	fmt.Println(os.Getenv("CMP_TOKEN"))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module GoPolling-NW
 
 go 1.9
+
+require (
+	github.com/TwiN/go-color v1.1.0 // indirect
+	github.com/nullwulf/loggly v0.0.0-20220928223524-974e4cfcfdda
+)
