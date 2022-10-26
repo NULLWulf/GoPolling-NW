8689224cc05cf743cc28e240b43eb4858839a60c
nullwulf <nwolf2@oswego.edu>
2022-10-26 10:17:20 -0400

    ~~

---
+++ nwolf2-ec2-build.sh
@@ -0,0 +1,2 @@
+## Docker build script
+sudo docker build --rm -f=Dockerfile -t nwolf2-poller:ec2 .
---
+++ nwolf2-run.sh
@@ -0,0 +1,2 @@
+## Script to launch docker container ##
+docker run -d -p 35671:8080 --env APP_TAG=Poller-EC2 nwolf2-poller:ec2
